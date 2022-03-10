16b14e22761a42857172cb527c59be4de788a3f9
Marija H <marijahorvatt3@gmail.com>
2022-03-10 20:00:02 +0100

    Comments updated

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,33 +1,67 @@
 <template>
-  <div id="container">
+  <div id="container" :style="nodeMargin">
     <div id="user-comment-info">
-      <div>by 
+      <div>
+        by
         <router-link :to="'/user/' + comment.by">{{ comment.by }}</router-link>
         <span> {{ comment.time }} ago </span>
       </div>
-      <div id="comment-text" v-html="comment.text">{{comment.text}}</div>
-      <div> comments: {{comment.kids}}</div>
+      <div id="comment-text" v-html="comment.text">{{ comment.text }}</div>
+      
+      <div v-if="comment.kids">comments: {{ comment.kids }}</div>
+      <button v-show="comment.kids" v-if="!toogleChildren" id="toggle" @click="toggleChildren"> show replies + </button>
     </div>
 
-<div v-if="comment.kids !== []">
-  <ul class="comment-replies">
-      <comment v-for="kid in comment.kids" :key="kid.id" :comment="kid"></comment>
-  </ul>
-</div>
-    
+    <div v-if="comment.kids" v-show="showChildren">
+      <ul class="comment-replies">
+        <comment
+          v-for="kid in comment.kids"
+          :key="kid.id"
+          :comment="kid"
+          :spacing="spacing + 10"
+        ></comment>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "comment",
+  data: function () {
+    return {
+      showChildren: false,
+    };
+  },
   props: ["comment"],
+  spacing: {
+    type: Number,
+    default: 0,
+  },
+
+  methods: {
+    toggleChildren() {
+      return this.showChildren = !this.showChildren;
+    }   
+  },
+
+  computed: {
+    nodeMargin() {
+      return {
+        "margin-left": `${this.spacing}px`,
+      };
+    },
+  },
 };
 </script>
 
 <style scoped>
 #user-comment-info {
-  border: 1px solid black;
+  border: 1px solid rgb(201, 197, 197);
+}
+
+#container {
+  margin-bottom: 5px;
 }
 
 .comment-replies {
--- src/views/Comments.vue
+++ src/views/Comments.vue
@@ -1,21 +1,36 @@
 <template>
   <div style="margin: 5px; background-color: white">
     <SpinnerComponent v-show="!isLoaded"></SpinnerComponent>
-    <div>
+    <div v-show="storyItem">
       <div id="main-story-info">
         <div id="title">
           <span class="highlight">{{ storyItem.title }}</span>
         </div>
         <div id="story-info">
-          <span><span class="highlight">{{ storyItem.score }}</span> points</span>
-          <span>by
-            <span class="highlight"><router-link :to="'/user/' + storyItem.by">{{storyItem.by}}</router-link></span></span>
-          <span>submited on:<span class="highlight">{{ storyItem.time}}</span></span>
-          <span><span class="highlight">{{ storyItem.descendants }}</span> comments</span>
+          <span
+            ><span class="highlight">{{ storyItem.score }}</span> points</span
+          >
+          <span
+            >by
+            <span class="highlight"
+              ><router-link :to="'/user/' + storyItem.by">{{
+                storyItem.by
+              }}</router-link></span
+            ></span
+          >
+          <span
+            >submited on:<span class="highlight">{{
+              storyItem.time
+            }}</span></span
+          >
+          <span
+            ><span class="highlight">{{ storyItem.descendants }}</span>
+            comments</span
+          >
         </div>
       </div>
 
-      <comment v-for="kid in kids" :key="kid.id" :comment="kid"></comment>
+      <comment v-for="kid in storyItem.kids" :key="kid.id" :comment="kid"></comment>
     </div>
   </div>
 </template>
@@ -29,9 +44,8 @@ export default {
   data: function () {
     return {
       storyItem: {},
-      kids: [],
       id: this.$route.params.storyId,
-      isLoaded: false,
+      isLoaded: false
     };
   },
   components: {
@@ -41,35 +55,50 @@ export default {
 
   methods: {
     getKids: function () {
+      this.storyItem = {}
       fetch(`https://hacker-news.firebaseio.com/v0/item/${this.id}.json`)
         .then((response) => response.json())
         .then((response) => (this.storyItem = response))
-        .then(() => (this.isLoaded = true))
         .then(() => {
-          this.storyItem.kids.forEach((x) => {
-            fetch(`https://hacker-news.firebaseio.com/v0/item/${x}.json`)
-              .then((data) => data.json())
-              .then((data) => this.kids.push(data));
-          });
-        });
+          return this.populateComments(this.storyItem).then(() => this.isLoaded = true)
+
+         
+          // this.storyItem.kids.forEach((x) => {
+          //   fetch(`https://hacker-news.firebaseio.com/v0/item/${x}.json`)
+          //     .then((data) => data.json())
+          //     .then((data) => {
+          //       this.kids.push(data);
+          //     })
+          //     .then(() => this.getReplies(this.kids))
+          // })
+        })
+    },
+
+    populateComments: async function (object) {
+      if (object.kids) {
+        for (let i = 0; i < object.kids.length; i++) {
+          let item = await fetch(`https://hacker-news.firebaseio.com/v0/item/${object.kids[i]}.json`).then((data) => data.json());
+          object.kids[i] = await item;
+          this.populateComments(object.kids[i]);
+        }
+      }
     },
   },
 
   created() {
     this.getKids();
-  },
+  }
 };
 </script>
 
 
 <style scoped>
 #main-story-info {
-    height: 150px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    font-size: 1.1em;
-
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 1.1em;
 }
 
 #story-info,
@@ -84,3 +113,5 @@ export default {
   color: #0d6efd;
 }
 </style>
+
+           
